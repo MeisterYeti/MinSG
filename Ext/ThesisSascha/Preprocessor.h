faeb43e17c2c9b9581b672743108b65318afad07
MeisterYeti <ZordanZ@gmx.de>
2014-01-25 17:54:21 +0100

    added Renderer and Preprocessor

---
+++ Ext/ThesisSascha/Preprocessor.h
@@ -0,0 +1,28 @@
+/*
+	This file is part of the MinSG library extension ThesisSascha.
+	Copyright (C) 2014 Sascha Brandt <myeti@mail.uni-paderborn.de>
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifdef MINSG_EXT_THESISSASCHA
+
+#ifndef PREPROCESSOR_H_
+#define PREPROCESSOR_H_
+
+namespace MinSG {
+namespace ThesisSascha {
+
+class Preprocessor {
+public:
+	Preprocessor();
+	virtual ~Preprocessor();
+};
+
+} /* namespace ThesisSascha */
+} /* namespace MinSG */
+
+#endif /* PREPROCESSOR_H_ */
+#endif /* MINSG_EXT_THESISSASCHA */
---
+++ Ext/ThesisSascha/Renderer.cpp
@@ -0,0 +1,27 @@
+/*
+	This file is part of the MinSG library extension ThesisSascha.
+	Copyright (C) 2014 Sascha Brandt <myeti@mail.uni-paderborn.de>
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifdef MINSG_EXT_THESISSASCHA
+#include "Renderer.h"
+
+namespace MinSG {
+namespace ThesisSascha {
+
+Renderer::Renderer() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Renderer::~Renderer() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ThesisSascha */
+} /* namespace MinSG */
+#endif /* MINSG_EXT_THESISSASCHA */
