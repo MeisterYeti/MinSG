cd53fa11911335d0ddf3424331f25392fd3a6824
MeisterYeti <ZordanZ@gmx.de>
2014-01-25 17:54:21 +0100

    added Renderer and Preprocessor

---
+++ Ext/ThesisSascha/Renderer.h
@@ -0,0 +1,27 @@
+/*
+	This file is part of the MinSG library extension ThesisSascha.
+	Copyright (C) 2014 Sascha Brandt <myeti@mail.uni-paderborn.de>
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifdef MINSG_EXT_THESISSASCHA
+#ifndef RENDERER_H_
+#define RENDERER_H_
+
+namespace MinSG {
+namespace ThesisSascha {
+
+class Renderer {
+public:
+	Renderer();
+	virtual ~Renderer();
+};
+
+} /* namespace ThesisSascha */
+} /* namespace MinSG */
+
+#endif /* RENDERER_H_ */
+#endif /* MINSG_EXT_THESISSASCHA */
