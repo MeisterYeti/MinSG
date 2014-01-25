ad21673d249332ea20c7925f4510b245bccf9863
MeisterYeti <ZordanZ@gmx.de>
2014-01-25 17:54:21 +0100

    added Renderer and Preprocessor

---
+++ Ext/ThesisSascha/Preprocessor.cpp
@@ -0,0 +1,29 @@
+/*
+	This file is part of the MinSG library extension ThesisSascha.
+	Copyright (C) 2014 Sascha Brandt <myeti@mail.uni-paderborn.de>
+
+	This library is subject to the terms of the Mozilla Public License, v. 2.0.
+	You should have received a copy of the MPL along with this library; see the
+	file LICENSE. If not, you can obtain one at http://mozilla.org/MPL/2.0/.
+*/
+
+#ifdef MINSG_EXT_THESISSASCHA
+
+#include "Preprocessor.h"
+
+namespace MinSG {
+namespace ThesisSascha {
+
+Preprocessor::Preprocessor() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Preprocessor::~Preprocessor() {
+	// TODO Auto-generated destructor stub
+}
+
+} /* namespace ThesisSascha */
+} /* namespace MinSG */
+
+#endif /* MINSG_EXT_THESISSASCHA */
