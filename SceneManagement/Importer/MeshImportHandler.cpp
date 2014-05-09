ad5153c56ec2c1e55c835164be12a184aec2fcb6
MeisterYeti <ZordanZ@gmx.de>
2014-05-09 23:57:15 +0200

    use mesh importer to load base64 encoded meshes

--- SceneManagement/Importer/MeshImportHandler.cpp
+++ SceneManagement/Importer/MeshImportHandler.cpp
@@ -10,13 +10,38 @@
 */
 #include "MeshImportHandler.h"
 #include "../../Helper/Helper.h"
+#include "../../Core/Nodes/GeometryNode.h"
+#include "../SceneDescription.h"
+
 #include <Util/IO/FileLocator.h>
+#include <Util/Macros.h>
+#include <Util/Encoding.h>
+
+#include <Rendering/Serialization/Serialization.h>
 
 namespace MinSG {
 namespace SceneManagement {
 
 Node * MeshImportHandler::handleImport(const Util::FileLocator& locator, const std::string & url, const NodeDescription * /*description*/) {
 	return loadModel(Util::FileName(url), 0, nullptr,locator);
+}
+
+Node * MeshImportHandler::handleImport(const NodeDescription * description) {
+	if(description->getValue(Consts::DATA_BLOCK) == nullptr)
+		return nullptr;
+	const std::string dataBlock = description->getString(Consts::DATA_BLOCK);
+	if(description->getString(Consts::ATTR_DATA_ENCODING) != Consts::DATA_ENCODING_BASE64) {
+		WARN("Unknown data block encoding.");
+		return nullptr;
+	}
+	const std::vector<uint8_t> meshData = Util::decodeBase64(dataBlock);
+
+	Rendering::Mesh * mesh = Rendering::Serialization::loadMesh("mmf", std::string(meshData.begin(), meshData.end()));
+	if(mesh == nullptr) {
+		WARN("Loading the mesh failed.");
+		return nullptr;
+	}
+	return new GeometryNode(mesh);
 }
 
 }
