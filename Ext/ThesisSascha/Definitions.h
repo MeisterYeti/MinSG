c956a59b5313525b7a025ba41a2883d9d70a7b20
MeisterYeti <ZordanZ@gmx.de>
2014-05-27 14:29:01 +0200

    fixed caching strategy

--- Ext/ThesisSascha/Definitions.h
+++ Ext/ThesisSascha/Definitions.h
@@ -17,6 +17,7 @@ namespace MinSG {
 namespace ThesisSascha {
 
 #define MAX_JOB_NUMBER 10
+#define INITIAL_POOL_SIZE 200
 
 typedef Util::WrapperAttribute<Util::StringIdentifier> StringIDAttribute_t;
 const std::string GATypeNameStringIdentifier("StringIdentifier");
@@ -24,12 +25,14 @@ const std::string GAStringIdentifierHeader("$[StringId]");
 
 static const Util::StringIdentifier SURFEL_ID("surfelId");
 static const Util::StringIdentifier SURFEL_STRINGID("surfelStrId");
-static const Util::StringIdentifier SURFELS("surfels");
 static const Util::StringIdentifier SURFEL_REL_COVERING("surfelRelCovering");
 
 static const Util::StringIdentifier MESH_ID("meshId");
 static const Util::StringIdentifier MESH_STRINGID("meshStrId");
 static const Util::StringIdentifier MESH_COMPLEXITY("meshComplexity");
+
+static const Util::StringIdentifier NODE_LEVEL("nodeLevel");
+static const Util::StringIdentifier CHILDREN_LOADED("childrenLoaded");
 
 }
 }
